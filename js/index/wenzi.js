52fe1637dec2eaba9b76d493b5b3dac1cb91959e
ainusers <979111986@qq.com>
2018-09-15 17:52:39 +0800

    Site updated: 2018-09-15 17:52:35

---
+++ js/index/wenzi.js
@@ -0,0 +1,32 @@
+﻿var clientWidth = $(window).width();
+var clientHeight = $(window).height();
+
+$(window).resize(function() {
+    var newWidth = $(window).width();
+    var newHeight = $(window).height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 50);
+		});
+		return this;
+	};
+})(jQuery);
